d0fa1df8ecc2efc31451158f1f41b1a9012c9df9
andreytarasyuk <workandreytarasyuk@gmail.com>
2017-11-16 05:19:59 +0200

    delete file

--- js/app.js
+++ js/app.js
@@ -20,55 +20,15 @@ $(document).ready(function() {
 		e.preventDefault();
 	});
 
-	$('.js-scroll-to').on("click", function(e){
-		e.preventDefault();
-		var anchor = $(this);
-		$('html, body').stop().animate({
-			scrollTop: $(anchor.attr('href')).offset().top
-		}, 1000);
-	});
-
-
-	// catalog__slider
-	// $('.popular-slider-js').slick({
-	// 	slidesToShow: 5,
-	// 	slidesToScroll: 1,
-	// 	arrows: true,
-	// 	responsive: [
-	// 	{
-	// 		breakpoint: 1200,
-	// 		settings: {
-	// 			slidesToShow: 4,
-	// 			slidesToScroll: 1,
-	// 			arrows: true
-	// 		}
-	// 	},
-	// 	{
-	// 		breakpoint: 992,
-	// 		settings: {
-	// 			slidesToShow: 3,
-	// 			slidesToScroll: 1,
-	// 			arrows: true
-	// 		}
-	// 	},
-	// 	{
-	// 		breakpoint: 715,
-	// 		settings: {
-	// 			slidesToShow: 2,
-	// 			slidesToScroll: 1,
-	// 			arrows: true
-	// 		}
-	// 	},
-	// 	{
-	// 		breakpoint: 520,
-	// 		settings: {
-	// 			slidesToShow: 1,
-	// 			slidesToScroll: 1,
-	// 			arrows: true
-	// 		}
-	// 	}
-	// 	]
+	// $('.js-scroll-to').on("click", function(e){
+	// 	e.preventDefault();
+	// 	var anchor = $(this);
+	// 	$('html, body').stop().animate({
+	// 		scrollTop: $(anchor.attr('href')).offset().top
+	// 	}, 1000);
 	// });
+
+
 });
 
 
